a5a154a711c4cb11f8fa2bba7fcf1e85054ce2f0
Rina <{ID}+{username}@users.noreply.github.com>
2022-12-05 13:57:36 +0300

    bar js added

--- js/bar.js
+++ js/bar.js
@@ -1,5 +1,5 @@
-let bar = document.querySelector('.bar');
-let navMenu = document.querySelector('.menu');
+const bar = document.querySelector('.bar');
+const navMenu = document.querySelector('.menu');
 
 bar.addEventListener('click', mobileMenu);
 
@@ -7,4 +7,13 @@ function mobileMenu() {
     bar.classList.toggle('active');
     navMenu.classList.toggle('active');
     cart.classList.remove('active')
-}
+};
+
+const navLink = document.querySelectorAll('.menu__item');
+
+navLink.forEach(e => e.addEventListener('click', closeMenu));
+
+function closeMenu() {
+    bar.classList.remove('active');
+    navMenu.classList.remove('active');
+};
