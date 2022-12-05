1e9185dfa985df7ede6898606967c31f3ca465e6
Rina <{ID}+{username}@users.noreply.github.com>
2022-12-05 15:11:18 +0300

    Updates

---
+++ js/bar.js
@@ -0,0 +1,19 @@
+const bar = document.querySelector('.bar');
+const navMenu = document.querySelector('.menu');
+
+bar.addEventListener('click', mobileMenu);
+
+function mobileMenu() {
+    bar.classList.toggle('active');
+    navMenu.classList.toggle('active');
+    cart.classList.remove('active');
+};
+
+const navLink = document.querySelectorAll('.menu__item');
+
+navLink.forEach(e => e.addEventListener('click', closeMenu));
+
+function closeMenu() {
+    bar.classList.remove('active');
+    navMenu.classList.remove('active');
+};
